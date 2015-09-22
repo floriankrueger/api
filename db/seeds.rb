8bbff2b4fcf8eef8804c2e932dce9f79712c3c2b
Florian Krüger <florian.krueger@projectserver.org>
2015-09-22 23:58:16 +0200

    add seed data for continents

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Continent.delete_all
+Continent.create!(:code => "af", :name => "Africa")
+Continent.create!(:code => "an", :name => "Antarctica")
+Continent.create!(:code => "as", :name => "Asia")
+Continent.create!(:code => "eu", :name => "Europe")
+Continent.create!(:code => "na", :name => "North America")
+Continent.create!(:code => "oc", :name => "Oceania")
+Continent.create!(:code => "sa", :name => "South America")
